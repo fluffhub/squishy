98e3304a567b67a815d4ee96d369f2f4313dff3a
William Wagner <william.wagner@gmail.com>
2016-10-24 03:57:45 -0700

    modifying fse placement

--- init.js
+++ init.js
@@ -273,7 +273,7 @@ except instead of defining HTML/XML markup you are writing function calls:
                 spoon.main.add(SquishyLogo);
             });
                        var browser=new FSe.FileBrowser("/var/www/html/squishy","poop7");
-                       spoon.main.add(browser);
+                       spoon.main.tasks.addTab(browser);
                        //M.Def("browser",browser);
                        console.debug({M:M});
                        //browser.Import("..");
