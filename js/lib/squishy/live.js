e36d36ad095150f131fa8bbba9e537ffb3291714
William Wagner <william.wagner@gmail.com>
2016-12-02 03:46:59 -0500

    fix live race

--- js/lib/squishy/live.js
+++ js/lib/squishy/live.js
@@ -1,5 +1,4 @@
 Module(function M () {
-  Import("squishy/system",function() {
   var loc = document.createElement("a")
   loc.href="#"
   var home=loc.href;
@@ -17,42 +16,44 @@ Module(function M () {
 
 
 
- // Import("squishy/system",function(system) {
- //   devices[root]=new system.Device(root);
+  // Import("squishy/system",function(system) {
+  //   devices[root]=new system.Device(root);
 
- // });
+  // });
 
   M.Def(function init(path,obj) {
-    var loc2=document.createElement("a");
-    loc2.href=path;
-    var fileloc=loc2.href;
-    loc2.href="/";
-    var fileroot=loc2.href;
-    var uri=fileloc.slice(fileroot.length)
-    var dirs=uri.split('/')
-    if(fileroot in devices) {
+    Import("squishy/system",function() {
+
+      var loc2=document.createElement("a");
+      loc2.href=path;
+      var fileloc=loc2.href;
+      loc2.href="/";
+      var fileroot=loc2.href;
+      var uri=fileloc.slice(fileroot.length)
+      var dirs=uri.split('/')
+      if(fileroot in devices) {
 
 
-    } else {
-      devices[fileroot]={};
-    }
-    var device=devices[fileroot];
-    var cursor=device;
-    for(var i=0;i<dirs.length-1;i++) {
-      var dn=dirs[i];
-
-      if(dn in cursor) {
-        cursor=cursor[dn]
       } else {
-        cursor[dn]={}
-        cursor=cursor[dn];
+        devices[fileroot]={};
       }
-    }
+      var device=devices[fileroot];
+      var cursor=device;
+      for(var i=0;i<dirs.length-1;i++) {
+        var dn=dirs[i];
 
-    cursor[dirs[dirs.length-1]]=obj;
+        if(dn in cursor) {
+          cursor=cursor[dn]
+        } else {
+          cursor[dn]={}
+          cursor=cursor[dn];
+        }
+      }
+
+      cursor[dirs[dirs.length-1]]=obj;
 
       devices[fileroot].push(fileloc.slice(fileroot.length));
 
-  });
+    });
   });
 });
