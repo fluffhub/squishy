03e28af0176ed9cf3cf43f8ffaf5712f5a2302ab
William Wagner <william.wagner@gmail.com>
2016-12-11 02:26:24 -0500

    update live

--- js/lib/squishy/live.js
+++ js/lib/squishy/live.js
@@ -1,20 +1,19 @@
 Module(function M () {
-  M.Import("squishy/system",function(system) {
-     M.Def("devices",new system.DeviceManager());
-var loc = document.createElement("a")
-  loc.href="#"
-  var home=loc.href;
-  M.Def("~",loc.href)
-  loc.href="/"
-  var root=loc.href;
-  M.Def("root",root);
+  Import("squishy/system",function(system) {
+    M.Def("devices",new system.DeviceManager());
+    var loc = document.createElement("a")
+    loc.href="#"
+    var home=loc.href;
+    M.Def("~",loc.href)
+    loc.href="/"
+    var root=loc.href;
+    M.Def("root",root);
 
-  //  console.debug({href:loc.href})
+    //  console.debug({href:loc.href})
 
-  Import("squishy/DOM", function(DOM){
-    M.Def("main",new DOM.Frame());
+    Import("squishy/DOM", function(DOM){
+      M.Def("main",new DOM.Frame());
+    });
   });
-   });
-
- });
+});
 
