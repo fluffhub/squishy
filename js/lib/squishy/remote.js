42acb5089359d7b35c09c50ce2a960d0dacee658
William Wagner <william.wagner@gmail.com>
2016-12-12 03:34:07 -0500

    fix remte

--- js/lib/squishy/remote.js
+++ js/lib/squishy/remote.js
@@ -1,6 +1,7 @@
 Module(function M() {
   Import("squishy/system",function(system) {
     M.Class(function C() {
+      C.Super(system.Device);
       C.Init(function Device(path,name) {
         system.Device.call(this,path,name);
         this.root=new system.Dir("/",{});
