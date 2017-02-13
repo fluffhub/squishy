fc62561f0ff053b0bd73c73d2ae9901b5d0c63b4
William Wagner <william.wagner@gmail.com>
2017-02-12 20:38:38 -0500

    fix logo

--- js/lib/squishy/svg.js
+++ js/lib/squishy/svg.js
@@ -79,7 +79,7 @@ M.Import('squishy/DOM',function(DOM) {
             var req=new Request("URI","TEXT").Get(src,{},function(svgtext) {
 
               var doc = new DOMParser().parseFromString(svgtext,"image/svg+xml");
-              SVGTag.call(tag,'svg',document.importNode(doc.documentElement, true);
+              SVGTag.call(tag,'svg',document.importNode(doc.documentElement, true));
               //tag.element=//
 
 
