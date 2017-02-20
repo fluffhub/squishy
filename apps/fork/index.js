0f0ff4fe72b076d7c69284a79b361621a31eb5b9
William Wagner <william.wagner@gmail.com>
2017-02-20 01:39:44 -0500

    fork updates

--- apps/fork/index.js
+++ apps/fork/index.js
@@ -34,7 +34,7 @@ Module(function M() {
 
         });
       })
-      var wrappers=new window.Module (function M2() {
+      var wrappers=window.Module (function M2() {
         var fliws={};
         M2.Def("FileListItemWrappers",fliws);
         var afliw=M2.Def(function addFileListItemWrapper(name,match,wrap,open) {
