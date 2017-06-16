65bcfbe2b94912dcb37d83ce4c0ba45c8f760f4a
William Wagner <william.wagner@gmail.com>
2017-06-16 02:45:39 -0400

    update fluffy

--- apps/fluffy/index.js
+++ apps/fluffy/index.js
@@ -1,5 +1,5 @@
 Module(function M() {
-  M.Import("fluffy/JSeditor","spoon",
+  M.Import("fluffy/JSeditor","spoon","basic","system","live","form", "spoon/windowing",
 
     //  context actions
     //  list view
@@ -7,7 +7,7 @@ Module(function M() {
     //  editor
     //  viewer
 
-    function (JSe,spoon) {
+    function (JSe,spoon,basic,system,live,form,windowing) {
        console.debug({"js editor loaded":JSe} );
       M.Def(function match(file,name) {
         if(typeof name=="string" && name.slice(-3)==".js") {
@@ -44,8 +44,57 @@ Module(function M() {
 
         });
       });
+      var CodeEditor=M.Class(function C() {
+        C.Super(windowing.AppContainer)
+        C.Init(function TextEditor(loc,items) {
+          windowing.AppContainer.call(this);
+          //basic.Div.call(this);
+          if (loc instanceof Element) {
+          } else {
+            loc=system.uri(loc)
+          }
+          var title=this.titlebar;
+          //new basic.Span(loc);
+          //title.addClass("title");
+          //this.add(title);
+          title.content(loc);
+          var ta=new form.TextBox();
+          ta.element.style.display="none";
+          this.add(ta);
+          var te=this;
+          Object.defineProperty(this,"File",{enumerable:false,writable:true,configurable:false})
+          this.value=null;
 
+          live.DeviceManager.retrieve(loc,function(results) {
+            Object.keys(results).forEach(function (name) {
+              var instance=results[name];
+              if(instance instanceof system.File) {
+                te.File=instance;
+                console.debug({fileinstance:instance});
+                te.File.read(function() {
+                  te.value=te.File.value;
+                  ta.value(te.value);
+                  te.onload();
+                });
+              }
+            });
+          });
+          this.addClass("CodeEditor");
+        });
+        C.Def(function onload() {
+          this.add(new JSe.JSEditor(this.value,spoon.main));
+        });
+      });
+      var live_items=[];
+      M.Def(function open(loc) {
+        if(loc in live_items) {
 
+        } else {
+          var te=new CodeEditor(loc);
+          live_items.push(te);
+          return te;
+        }
 
+      });
     });
 });
