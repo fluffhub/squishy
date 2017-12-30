91300a1db67704ce276ce7ef9e8a36252ad2c4ae
William Wagner <william.wagner@gmail.com>
2017-12-29 23:28:44 -0500

    update console

--- apps/Console.js
+++ apps/Console.js
@@ -10,7 +10,7 @@ Module(function M() {
            "squishy/system",
            function(DOM,Req,form,windowing,membrane,system) {
              console.debug("imported");
-               document.styleSheets[0].addRule(".Console>textarea","width: 100%;clear: both;min-height: 80%;");
+             document.styleSheets[0].addRule(".Console>textarea","width: 100%;clear: both;min-height: 80%;");
 
              var Commander=M.Class(function C() {
                C.Def("session",null);
@@ -18,37 +18,57 @@ Module(function M() {
                C.Def("id","pool")
                C.Super(windowing.AppContainer);
                C.Init(function  Commander(id) {
-                 this.session=new membrane.Device(system.uri("").href)
+                 try {
+                   this.session=new membrane.Device(system.uri("").hostname)
+                 } catch (e) {
+                   this.session=new membrane.DeviceManager.devices[system.uri("").hostname].membrane;
+                 }
                  windowing.AppContainer.call(this);
+                 this.titlebar.content("Membrane Console");
                  this.request=new Req.Request("URI","TEXT");
                  this.request.request.timeout=60000;
+                 var commander=this;
+
                  if(id!==undefined) {
                    this.id=id;
                  } else {
                    this.id="pool";
                  }
 
-                 //var commander=new Commander("pool1");
-                 var commander=this;
                  Import("squishy/form",function(form) {
-                   var input=new form.TextInput("input","",function() {
 
-                   });
+                   this.selector=new form.Selector();
+                   Import("squishy/live",function(live) {
+                     var manager=live.DeviceManager;
+                     var devnames=Object.keys(manager.devices);
+                     for(var i=0;i<devnames.length;i++) {
+                       var devname=devnames[i];
 
-                   var output=new form.TextBox("output")
+                     }
 
-                   var submit=new form.Submit();
 
-                   var form=new form.Form("Console",function(e) {
-                     e.preventDefault();
-                     commander.send(input.value(),function(result) {
-                       console.debug("received:"+result);
-                       input.content(input.content()+result);
+                     //var commander=new Commander("pool1");
+
+                     var input=new form.TextInput("input","",function() {
+
                      });
+                     var output=new form.TextBox("output")
+
+                     var submit=new form.Submit();
+
+                     var form=new form.Form("Console",function(e) {
+                       e.preventDefault();
+                       commander.send(input.value(),function(result) {
+                         console.debug("received:"+result);
+                         input.content(input.content()+result);
+                       });
+                     });
+
+                     form.Madd(output,input,submit);
+                     commander.add(form);
                    });
 
-                   form.Madd(output,input,submit);
-                   commander.add(form);
+
                  });
                });
 
