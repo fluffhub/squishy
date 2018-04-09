c0995227059a398951438a14252e6b48779783f1
William Wagner <william.wagner@gmail.com>
2018-04-08 23:54:01 -0400

    update ticker

--- apps/ticker/fake.js
+++ apps/ticker/fake.js
@@ -1,6 +1,24 @@
 Module(function M() {
   M.Import("squishy/realtime",function(rt) {
-
+    function PseudoRandomSymbol(n,a) {
+      var P=Math.PI;
+      var r=Math.round(128+255*Math.sin((n*P/7)+(2*P/3))/2);
+      var g=Math.round(128+255*Math.sin((n*P/7)+(4*P/3))/2);
+      var b=Math.round(128+255*Math.sin((n*P/7)+(2*P))/2);
+      if(a!==undefined) { a = Math.round(a*100)/100 }
+      else a=1;
+      return "rgba("+r+","+g+","+b+","+a+")";
+    }
+    var RandomSymbol=M.Def(function RandomSymbol() {
+      var max=456976;
+      var id="";
+      var n=10000;
+      for (var i=0;i<3;i++) {
+        var chars=Math.round(Math.random()*max/n)*n;
+        id+=String.fromCharCode(97 + chars%26);
+        chars=chars/26;
+      }
+    });
     var FakeSocket=M.Class(function C() {
       C.Init(function FakeSocket() {
         this.past_ids=[];
@@ -9,44 +27,41 @@ Module(function M() {
         this.max_initial_value=1000;
       });
       C.Def(function send(value) {
-        if(value=="start") {
+        var keys=value.split(" ");
+        if(keys[0]=="start") {
           this.active=true;
-          this.start_messaging();
+          this.start_messaging(keys[1]);
         }
       });
-      C.Def(function start_messaging() {
-        var max=456976;
-        var avgtime=500;
-        var n=100;
+      C.Def(function start_messaging(id, min, max, freq) {
+
+        var mintime=(1.0/freq)*1000;
+        var maxtime=50;
+
         var lt=performance.now();
-        while(this.active) {
-          var id="";
-          var value;
-          for (var i=0;i<3;i++) {
-            var chars=Math.round(Math.random()*max/n)*n;
-            id+=String.fromCharCode(97 + chars%26);
-            chars=chars/26;
-          }
-          var pn = this.past_ids.indexOf(id);
-          if(pn>-1) {
-            var speed=Math.round(Math.random()*this.max_speed);
-            var sign=Math.round(Math.random()*10000)>5000?-1:1;
-            var tpv=this.past_values[pn];
+        var pn = this.past_ids.indexOf(id);
+        if(pn==-1) {
+          this.past_ids.push(id);
+          this.past_values.push([Math.round(Math.random()*this.max_initial_value)]);
+          pn=0;
 
+          var speed, sign, tpv, value, nt;
+          while(this.active) {
+            speed=Math.round(Math.random()*max);
+            sign=Math.round(Math.random()*10000)>4999?-1:1;
+            tpv=this.past_values[pn];
             value=tpv[tpv.length-1]+speed*sign;
+            if (tpv.length > 1000)
+              tpv.shift();
             tpv.push(value);
-            if (tpv.length > 10000) tpv.shift()
 
-          } else {
-            this.past_ids.push(id);
-            value=Math.round(Math.random()*this.max_initial_value);
-            this.past_values.push([value]);
-            pn=0;
+            nt=lt+(mintime);//*1000;
+
+            while(lt<nt) {
+              lt=performance.now();
+            }
+            this.onmessage({id:value});
           }
-          setTimeout(function() {
-          this.onmessage(JSON.stringify({id:id, value:value}));
-          },Math.round(avgtime*Math.random()))
-
         }
       });
       C.Def(function stop_messaging() {
@@ -60,7 +75,7 @@ Module(function M() {
     M.Class(function C() {
       C.Super(rt.Remote);
       C.Init(function Remote() {
-        rt.Remote.call(this)
+        rt.Remote.call(this);
         this.socket=new FakeSocket();
       });
     });
