76038b0afeb497ec1ed85395a2c96d34e4a407a2
William Wagner <william.wagner@gmail.com>
2018-04-11 23:15:26 -0400

    update chart

--- apps/ticker/index.js
+++ apps/ticker/index.js
@@ -34,7 +34,6 @@ Module(function M() {
             this.st=performance.now();
             this.lt=this.st;
             this.starttime=0;
-
             this.lp=0;
           });
           C.Def(function resetSize() {
@@ -50,7 +49,7 @@ Module(function M() {
             if(this.def.points.length>100) {
               this.def.points.splice(1,1);
               this.def.commands.splice(1,1);
-              this.starttime=(this.starttime+dt);
+              this.starttime=(this.maxtime-this.timewidth);
             }
             this.def.addPoint("L",[[this.maxtime,value]]);
             if(this.max==null||value>this.max) {
