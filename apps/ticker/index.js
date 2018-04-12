6146ae5edf6849fb0fb8e22af9ccda120ff7bbe8
William Wagner <william.wagner@gmail.com>
2018-04-11 22:47:09 -0400

    update chart

--- apps/ticker/index.js
+++ apps/ticker/index.js
@@ -7,36 +7,7 @@ Module(function M() {
               svg,form,windowing,
               interactive) {
       M.Index("fake");
-      document.styleSheets[0].addRule(
-        ".button.addticker.enabled", ""+
-        "width: 1em;"+
-        "height: 1.3em;"+
-        "line-height: 1.3em;"+
-        "display: inline-block;"+
-        "text-align: center;"
-      )
-      document.styleSheets[0].addRule(".ticker",
-                                      "padding:0.1em 0.5em;");
-      document.styleSheets[0].addRule(".ticker svg",
-                                      "stroke-width:5px;stroke:orange;height:2.5em;width:100px;");
-      document.styleSheets[0].addRule(".tickerentry",
-                                      "padding:0.5em;");
-      document.styleSheets[0].addRule(".ticker .sym",
-                                      "line-height:1.25em;font-family:monospace;font-size:1.25em;");
-      document.styleSheets[0].addRule(".ticker .sym",
-                                      "line-height:1.25em;font-family:monospace;");
-            document.styleSheets[0].addRule(".vals span.new",
-                                            "transition:unset;");
-      document.styleSheets[0].addRule(".ticker .info",
-                                      "display:inline-block;");
-      document.styleSheets[0].addRule(".vals",
-                                      "position:relative;overflow:hidden;font-family:monospace;width:4em;height:1.25em;font-size:1em;");
-      document.styleSheets[0].addRule(".vals span",
-                                      "transform:translateZ(0);transition:top 0.2s linear, color 1s linear 0.5s;top:0;color:black;position:absolute;");
-      document.styleSheets[0].addRule(".vals span.down",
-                                      "top:-1.25em;color:red;");
-      document.styleSheets[0].addRule(".vals span.up",
-                                      "top:1.25em;color:green;");
+
       Import("ticker/fake",function(fake) {
 
         var MiniChart=M.Class(function C() {
@@ -73,17 +44,22 @@ Module(function M() {
             var tn = performance.now();
             var dt=tn-this.st;
             this.maxtime=dt*this.timescale;
-
+            if(this.def.points.length>100) {
+              this.def.points.splice(1,1);
+              this.def.commands.splice(1,1);
+            }
             this.def.addPoint("L",[[this.maxtime,value]]);
             if(this.max==null||value>this.max) {
               this.max=value;
-       //       this.resetSize();
+              //       this.resetSize();
             }
             if(this.min==null||value<this.min) {
               this.min=value;
-           //   this.resetSize();
+              //   this.resetSize();
             }
+
             this.resetSize();
+
 
             this.lt=tn;
             this.path.define(this.def);
@@ -92,7 +68,6 @@ Module(function M() {
         var Ticker=M.Class(function C() {
           C.Super(basic.Div);
           C.Init(function Ticker(symbol, socket) {
-
             basic.Div.call(this,"ticker");
             var ticker=this;
             this.id=symbol;
@@ -125,16 +100,17 @@ Module(function M() {
                 //if(ticker.Value.oldValue) ticker.Value.oldValue.remove();
                 //delete ticker.Value.oldValue;
                 //ticker.Value.add(newValue);
-                setTimeout(function() {
-                  newValue.removeClass("new");
-                  newValue.removeClass("up");
-                  newValue.removeClass("down");
-                }, 0);
-                ticker.Value.removeClass("updated");
                 //ticker.Value.oldValue=newValue;
                 if(v.value>ticker.max) ticker.max=v.value;
                 if(v.value<ticker.min) ticker.min=v.value;
                 ticker.Chart.addPoint(v.value)
+                setTimeout(function() {
+                  newValue.removeClass("new");
+                  newValue.removeClass("up");
+                  newValue.removeClass("down");
+                  ticker.Value.removeClass("updated");
+                }, 0);
+
               }
             });
           });
@@ -142,7 +118,6 @@ Module(function M() {
           var TickerMaker=M.Class(function C() {
             C.Super(windowing.AppContainer);
             C.Init(function() {
-
               windowing.AppContainer.call(this,"tickermaker");
               this.titlebar.content("tickers");
               var tm=this;
@@ -175,4 +150,34 @@ Module(function M() {
         });
       });
     });
+  document.styleSheets[0].addRule(
+    ".button.addticker.enabled", ""+
+    "width: 1em;"+
+    "height: 1.3em;"+
+    "line-height: 1.3em;"+
+    "display: inline-block;"+
+    "text-align: center;"
+  )
+  document.styleSheets[0].addRule(".ticker",
+                                  "padding:0.1em 0.5em;");
+  document.styleSheets[0].addRule(".ticker svg",
+                                  "stroke-width:5px;stroke:orange;height:2.5em;width:100px;");
+  document.styleSheets[0].addRule(".tickerentry",
+                                  "padding:0.5em;");
+  document.styleSheets[0].addRule(".ticker .sym",
+                                  "line-height:1.25em;font-family:monospace;font-size:1.25em;");
+  document.styleSheets[0].addRule(".ticker .sym",
+                                  "line-height:1.25em;font-family:monospace;");
+  document.styleSheets[0].addRule(".vals span.new",
+                                  "transition:unset;");
+  document.styleSheets[0].addRule(".ticker .info",
+                                  "display:inline-block;");
+  document.styleSheets[0].addRule(".vals",
+                                  "position:relative;overflow:hidden;font-family:monospace;width:4em;height:1.25em;font-size:1em;");
+  document.styleSheets[0].addRule(".vals span",
+                                  "transform:translateZ(0);transition:top 0.2s linear, color 1s linear 0.5s;top:0;color:black;position:absolute;");
+  document.styleSheets[0].addRule(".vals span.down",
+                                  "top:-1.25em;color:red;");
+  document.styleSheets[0].addRule(".vals span.up",
+                                  "top:1.25em;color:green;");
 });
