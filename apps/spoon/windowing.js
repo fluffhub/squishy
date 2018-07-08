3fcd9d5f09a859b40637dbccf1d0497874c56cd7
William Wagner <william.wagner@gmail.com>
2018-07-08 03:09:26 -0400

    update windowing

--- apps/spoon/windowing.js
+++ apps/spoon/windowing.js
@@ -17,6 +17,7 @@ Module(function M() {
       var AppContainer=M.Class(function C() {
         C.Super(transform.HTMLPositionBox);
         C.Mixin(transform.Resizable);
+        C.Mixin(transform.Draggable);
         C.Mixin(events.HasEvents);
         //C.Mixin(transform.Draggable);
         C.Init(function AppContainer(parent) {
@@ -30,8 +31,7 @@ Module(function M() {
           //titlebar
           this.titlebar=new basic.Div("acb")
           this.add(this.titlebar);
-          this.titlebar.Mixin(transform.Draggable);
-
+          
           this.size={width:400,height:300};
           this.drawTransform();
 
@@ -54,14 +54,14 @@ Module(function M() {
 
           //draggable
           var acw=this;
-          this.titlebar.enabledrag(function ondrag(i,v) {
+          this.enabledrag(function ondrag(i,v) {
 
             //// acw.element.style.top=v.position.y+"px";
             // acw.element.style.left=v.position.x+"px";/
-            i.parent.position.x=i.parent.position.x+i.delta.x
-            i.parent.position.y=i.parent.position.y+i.delta.y
-          
-            i.parent.drawTransform();
+          ///  i.parent.position.x=i.parent.position.x+i.delta.x
+          //  i.parent.position.y=i.parent.position.y+i.delta.y
+            i.drawTransform();
+          //  i.parent.drawTransform();
             //console.debug({x:v.position.x,y:v.position.y});
           });
           //resizable
