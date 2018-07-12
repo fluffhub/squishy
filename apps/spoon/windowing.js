f5ab425263eb17920ca7d5d9b632eb2b18f38683
William Wagner <william.wagner@gmail.com>
2018-07-12 01:23:10 -0400

    update windowing

--- apps/spoon/windowing.js
+++ apps/spoon/windowing.js
@@ -67,7 +67,7 @@ Module(function M() {
           },this.titlebar.element);
           //resizable
           ///console.debug(this);
-          this.enableresize("nw,n,ne,e,se,s,sw,w",function onresize(item) {
+          this.enableresize("n,e,s,w,nw,ne,se,sw",function onresize(item) {
            
           });
 
