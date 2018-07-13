511cb1a3e638499742fe73d6499352ce36466275
William Wagner <william.wagner@gmail.com>
2018-07-13 00:40:02 -0400

    update text

--- apps/Console.js
+++ apps/Console.js
@@ -19,7 +19,7 @@ Module(function M() {
         S.addRule(".Console .acc form input[type=\"submit\"]", "flex:0 0 3.5em;border:none; display:block;width:3em;bottom:0px;line-height:1.5em;font-family:monospace serif;");
         S.addRule(".Console .acc form input:first-child",{flex:"1 1 auto", "line-height":"1.5em", "text-indent":"0.5em", border:"none","font-family":"monospace, serif"});
        
-        S.addRule(".Console .acc .tty p","padding:0 0.5em; display:inline-block;float:left;clear:both;");
+        S.addRule(".Console .acc .tty .trans","padding:0 0.5em; display:inline-block;float:left;clear:both;");
         S.addRule(".Console .acc .tty", "padding:0.5em 0;position:absolute;left:0;right:0;top:0;bottom:1.5em;overflow:scroll;font-family:monospace serif;")
 
         S.addRule(".Console","position: absolute;");
