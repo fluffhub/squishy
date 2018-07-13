6cc54e3ee76b7813f80dc07c3333b26f206d0d08
William Wagner <william.wagner@gmail.com>
2018-07-13 00:58:11 -0400

    update text

--- apps/Console.js
+++ apps/Console.js
@@ -14,7 +14,7 @@ Module(function M() {
     function(DOM,Req,form,membrane,system,live,basic,styles,spoon,windowing) {
       M.Style(function S() {
         S.addRule(".Console .acc form ", "position:absolute;bottom:0;height:1.5em;width:100%;display:flex;")
-        S.addRule(".Console .acc textarea",{width: "100%",clear: "both",border:"none", "background-color":"#eee"});
+        S.addRule(".Console .acc textarea",{width: "100%",clear: "both",border:"none", "background":"none"});
         S.addRule(".Console .acc .trans",{display: "block",width:"100%",position: "relative",'box-sizing': "border-box"});
         S.addRule(".Console .acc form input[type=\"submit\"]", "flex:0 0 3.5em;border:none; display:block;width:3em;bottom:0px;line-height:1.5em;font-family:monospace serif;");
         S.addRule(".Console .acc form input:first-child",{flex:"1 1 auto", "line-height":"1.5em", "text-indent":"0.5em", border:"none","font-family":"monospace, serif"});
